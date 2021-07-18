2dea4d963e3431d2390d3140da0da89ba93fd443
dmitriysemenko <dm123semenko@gmail.com>
2021-07-18 19:28:21 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>App Component</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+        </div>
+    );
 }
 
 export default App;
