aa1a627a01e7850bd24fa905a12d3cae887e62c9
dmitriysemenko <dm123semenko@gmail.com>
2021-07-18 23:03:53 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
     return (
         <div className="App">
+            <Navbar />
+            <div className="content">
+                <Home />
+            </div>
         </div>
     );
 }
