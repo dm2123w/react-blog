2efd3a2842c79973aca238f26474a4ac5e15f08a
dmitriysemenko <dm123semenko@gmail.com>
2021-07-20 23:03:54 +0000

    update

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,20 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [name, setName] = useState("Mario");
+    const [age, setAge] = useState(25);
+
     const handleClick = (e) => {
-        console.log("Hello Ninjas!", e);
+        setName("Luigi");
+        setAge(30);
     };
 
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>
+                {name} is {age} years old
+            </p>
             <button onClick={handleClick}>Click me</button>
         </div>
     );
