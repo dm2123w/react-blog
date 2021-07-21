c832f4ea5419cf34d9fe038edbdc4c1488fa86e8
dmitriysemenko <dm123semenko@gmail.com>
2021-07-21 21:36:56 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
-import Post from "./Post";
 
 function App() {
     return (
         <div className="App">
             <Navbar />
-            <div className="content">
-                <Home />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-            </div>
+            <div className="content"></div>
+            <Home />
         </div>
     );
 }
--- src/Post.js
+++
@@ -1,5 +0,0 @@
-const Post = () => {
-    return <div>This is a new post.</div>;
-};
-
-export default Post;
