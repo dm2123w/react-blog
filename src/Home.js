bf3d03531462ccb25d4cf964ddd3cfb56e78e61f
dmitriysemenko <dm123semenko@gmail.com>
2021-07-22 20:30:33 +0100

    update

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
@@ -21,14 +22,10 @@ const Home = () => {
             id: 3,
         },
     ]);
+
     return (
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2> {blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} />
         </div>
     );
 };
--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ const Navbar = () => {
         <nav className="navbar">
             <h1>My Blog</h1>
             <div className="links">
-                <a href="/" className="home">Home</a>
+                <a href="/" className="home-button">Home</a>
                 <a
                     href="/create" className="new-blog"
                 >
