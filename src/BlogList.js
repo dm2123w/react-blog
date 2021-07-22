2b1f774789efa89bb6081b30326059c55515efd0
dmitriysemenko <dm123semenko@gmail.com>
2021-07-22 23:57:30 +0100

    update

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,12 +1,14 @@
-const BlogList = (props) => {
-    const blogs = props.blogs;
-
+const BlogList = ({ blogs, title, handleDelete }) => {
     return (
         <div className="blog-list">
+            <h2>{title}</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2> {blog.title}</h2>
                     <p>Written by {blog.author}</p>
+                    <button onClick={() => handleDelete(blog.id)}>
+                        Delete blog
+                    </button>
                 </div>
             ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -23,9 +23,18 @@ const Home = () => {
         },
     ]);
 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter((blog) => blog.id !== id);
+        setBlogs(newBlogs);
+    };
+
     return (
         <div className="home">
-            <BlogList blogs={blogs} />
+            <BlogList
+                blogs={blogs}
+                title="All Blogs"
+                handleDelete={handleDelete}
+            />
         </div>
     );
 };
