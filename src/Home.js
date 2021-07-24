9df03190ef34c54dce9940b386381bc5552fd51c
dmitriysemenko <dm123semenko@gmail.com>
2021-07-24 20:14:09 +0000

    update

--- src/Home.js
+++ src/Home.js
@@ -3,19 +3,32 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch("http://localhost:8000/blogs")
             .then((res) => {
+                if (!res.ok) {
+                    throw Error("Could not fetch the data for that resource");
+                }
                 return res.json();
             })
             .then((data) => {
                 setBlogs(data);
+                setIsLoading(false);
+                setError(null);
+            })
+            .catch((err) => {
+                setError(err.message);
+                setIsLoading(false);
             });
     }, []);
 
     return (
         <div className="home">
+            {error && <div>{error}</div>}
+            {isLoading && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
     );
