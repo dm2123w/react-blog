03caa93787d2ff4dbe14d88595fad7d58614b311
dmitriysemenko <dm123semenko@gmail.com>
2021-07-28 21:11:09 -0100

    update

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,25 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams();
+    const {
+        data: blog,
+        error,
+        isLoading,
+    } = useFetch("http://localhost:3000/blogs/" + id);
 
     return (
         <div className="blog-details">
-            <h2>Blog Details - {id}</h2>
+            {isLoading && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            )}
         </div>
     );
 };
