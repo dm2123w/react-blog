47eabacfe22004809ed77ffe07c14ed0759a0cda
dmitriysemenko <dm123semenko@gmail.com>
2021-07-29 23:29:41 -0100

    update

--- src/Create.js
+++ src/Create.js
@@ -1,18 +1,40 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("mario");
+
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
             <form>
                 <label>Blog title:</label>
-                <input type="text" required />
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
                 <label>Blog body:</label>
-                <textarea required></textarea>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
                 <label>Blog author:</label>
-                <select>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
                     <option value="mario">Mario</option>
                     <option value="yoshi">Yoshi</option>
                 </select>
                 <button>Add Blog</button>
+                <p>Preview:</p>
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
             </form>
         </div>
     );
